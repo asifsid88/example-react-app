47d05968fea4840307fc596f1b10c3f39e43dd20
Ashique Hussain <mhussaa@amazon.com>
2017-08-15 15:41:44 +0530

    React-redux set up

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <h1>
+    React webapp is up and running!!
+  </h1>, document.getElementById('root'));
